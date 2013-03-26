f9c191a1dbd5c5f8cc61476993e1f0684a7bf0be
Adeeb <adeeb.umar@strath.ac.uk>
2013-03-26 17:13:07 +0000

    accplot plots all if database raw data. Filter plots specific data that has been filtered.

--- Matlab_Processing/FilterData.m
+++ Matlab_Processing/FilterData.m
@@ -1,28 +1,23 @@
 javaaddpath 'mysql-connector-java-5.1.24-bin.jar';
 conn=database('samples', 'root', '', 'com.mysql.jdbc.Driver', 'jdbc:mysql://localhost/');
-curs = exec(conn,'SELECT * FROM activity');
+curs = exec(conn,...
+    'SELECT * FROM sample where activity_type = ''s-w-s''and activity_id = 19');
 setdbprefs('DataReturnFormat','structure');
 curs = fetch(curs);
-rows = length(getfield(curs.Data,'id'));
-names = getfield(curs.Data,'activity_name');
+id = getfield(curs.Data,'id');
+x = getfield(curs.Data,'x');
+y = getfield(curs.Data,'y');
+z = getfield(curs.Data,'z');
+timestamp = getfield(curs.Data,'timestamp');
+close(conn);
 
-for i=1:19,
-    base = 'SELECT * FROM sample Where activity_id = ';
-    rowString = num2str(i);
-    query = strcat(base,rowString);
-    curs = exec(conn,query);
-    setdbprefs('DataReturnFormat','structure');
-    curs = fetch(curs);
-    %name = names(i)
-    id = getfield(curs.Data,'id');
-    x = getfield(curs.Data,'x');
-    y = getfield(curs.Data,'y');
-    z = getfield(curs.Data,'z');
-    timestamp = getfield(curs.Data,'timestamp');
-    ticknum = length(id);
-    ticknum = round(ticknum/20);
-    time = datenum( timestamp, 'yyyy-mm-dd HH:MM:SS.FFF');
-    %time2 = datestr(timestamp,'HH:MM:SS.FFF');
+filterx = smooth(x, 15, 'moving');
+filtery = smooth(y, 15, 'moving');
+filterz = smooth(z, 15, 'moving');
+ticknum = length(id);
+ticknum = round(ticknum/20);
+time = datenum( timestamp, 'yyyy-mm-dd HH:MM:SS.FFF');
+%time2 = datestr(timestamp,'HH:MM:SS.FFF');
 
 %# centimeters units
 X = 21.0;                  %# A3 paper size
@@ -35,14 +30,14 @@ ySize = Y - yMargin;     %# figure size on paper (widht & hieght)
 %# create figure/axis
 fig = figure('Menubar','none');
 
-plot(time,x,'x',time,y,'x',time,z,'x');
+plot(time,filterx,'x',time,filtery,'x',time,filterz,'x');
 datetick('x','HH:MM:SS');
 set(gca,'XTick', time(1:ticknum:end),'XTickLabel', timestamp(1:ticknum:end),...
-'Units','normalized');
+    'Units','normalized');
 %set(gca,'XTickLabel', timestamp(1:20:end));
 xticklabel_rotate([],45,[]);
 hleg = legend('x axis','y axis','z axis',...
-              'Location','NorthEastOutside');
+    'Location','NorthEastOutside');
 % Make the text of the legend italic and color it brown
 set(hleg,'FontAngle','italic','TextColor',[.3,.2,.1]);
 title('Three Axis Accelerometer Data against Time');
@@ -57,14 +52,10 @@ set(gcf, 'PaperUnits','centimeters');
 set(gcf, 'PaperSize',[X Y]);
 set(gcf, 'PaperPosition',[xMargin yMargin xSize ySize]);
 set(gcf, 'PaperOrientation','portrait');
-name = num2str(i);
-name = strcat('output',name);
-filename = strcat(name,'.pdf');
+
+filename = 'filter.pdf';
 %# export to PDF and open file
 %print -dpdf -r0 saveas
-%print(fig, '-dpdf', saveas) ;  
+%print(fig, '-dpdf', saveas) ;
 saveas(gcf, filename); %Save figure
-end
 
-
-close(conn);
