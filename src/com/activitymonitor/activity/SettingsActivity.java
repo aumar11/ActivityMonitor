0f7b7890b7d4fd4d8ff55f44ad3d41bc18f47a15
Adeeb <adeeb.umar@strath.ac.uk>
2013-03-30 19:32:39 +0000

    Adding implementation to Service Activity

--- src/com/activitymonitor/activity/SettingsActivity.java
+++ src/com/activitymonitor/activity/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.activitymonitor.activity;
 
+import java.util.regex.Pattern;
+
 import com.activitymonitor.R;
 import com.activitymonitor.R.layout;
 import com.activitymonitor.R.menu;
@@ -8,6 +10,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
@@ -28,6 +32,23 @@ public class SettingsActivity extends Activity {
 		return true;
 	}
 
+		public void onClick(View view){
+			EditText  host = (EditText)findViewById(R.id.edithost);
+			EditText  port = (EditText)findViewById(R.id.editport);
+			String patternhost = "^[\\d]{1,3}[\\.][\\d]{1,3}[\\.][\\d]{1,3}[\\.][\\d]{1,3}$";
+			String patternport = "^[\\d]+$";
+			switch(view.getId()){
+			case R.id.btnserversetup:
+				if(host.getText().toString().matches(patternhost) && port.getText().toString().matches(patternport)){
+					//set preferences and and go back to the main activity
+					//change set up to 1
+				}
+				else{
+					//show alert message
+					
+				}
+			}
+		}
 
 
 }
